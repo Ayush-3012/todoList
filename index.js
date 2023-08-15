a2bef59d9a3cd7539d335e92d7109548ed02f109
Ayush-3012 <diveshsrivastava31@gmail.com>
2023-08-15 18:53:23 +0530

    add mongodb database to store todo list items

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,13 @@ let todoList2 = new Array();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+mongoose.connect("mongodb://127.0.0.1:27017/todoListDB");
+
+const todoSchema = {
+  name: String,
+};
+
+const Todo = mongoose.model("Todo", todoSchema);
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
@@ -15,10 +23,23 @@ app.get("/", (req, res) => {
 
 app.post("/submit", (req, res) => {
   if (req.body["todo"] != "") {
-    const todo_item = req.body["todo"];
+    const item = new Todo({
+      name: req.body["todo"],
+    });
+
+    Todo.insertMany([item])
+      .then(function () {
+        mongoose.connection.close();
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+
+    const todo_item = item.name;
     todoList.push(todo_item);
     todoList2 = todoList.reverse();
   }
+
   res.render("index.ejs", { todoList: todoList2 });
 });
 
